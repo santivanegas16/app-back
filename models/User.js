2cbcfae73da8c18fcfb3473a6940d1b3503c91bf
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-05-15 21:11:04 -0500

    users

---
+++ models/User.js
@@ -0,0 +1,17 @@
+import { timeStamp } from "console";
+import { Schema, model, Types} from "mongoose"
+
+let collection = "users"
+
+let schema = new Schema({
+    email: {type: String, required: true},
+    password:{type: String, required: true},
+    photo:{type: String, required: true},
+    role:{type: Number, required: true},
+    online:{type: Boolean, required: true}
+},{
+    timestamps: true
+})
+
+let User = model(collection, schema)
+export default User
