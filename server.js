7fb66ce1bc26df7505292a1832dc19d12fd515eb
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-05-15 19:36:54 -0500

    minga-02

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 import express from "express";
-
 import "dotenv/config.js";
+import "./config/database.js"
 
 const server = express(); // Creo servidor
-
 const PORT = process.env.PORT || 8080; // Defino puerto
-
 const ready = () => console.log("ready on: " + PORT); // Callback
 
+//middleware
+server.use(express.json()) //permite entradas y trabajar con formato json
+server.use(express.urlencoded({ extended: false })) //permite capturar consultas complejas
+
+//Router + server
 server.listen(PORT, ready); //Iniciar servidor
 
 console.log(process.env.NODE_ENV)
