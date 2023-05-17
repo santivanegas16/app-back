caac9fb7cb74d7e69643a119f0a21aea4ec5d9aa
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-05-16 19:53:52 -0500

    model authors

---
+++ models/Author.js
@@ -0,0 +1,22 @@
+import { Schema, model, Types} from "mongoose"
+
+let collection = "autors"
+
+let schema = new Schema({
+    name: {type: String, required: true},
+    last_name:{type: String, required: false}, //por default el required esta en false se puede evitar ponerlo
+    city:{type: String, required: true},
+    country:{type: String, required: true},
+    date:{type: Date},
+    photo:{type: String, required: true},
+    user_id:{
+        type: Types.ObjectId,   // tipo de dato especial de mongo que es un string con propiedades extensivas de objetos
+        ref:"users",  // nombre de la collection con la que se tiene que relacionar
+        required: true},
+    active:{ type: Boolean, required: true}
+},{
+    timestamps: true
+})
+
+let Author = model(collection,schema)
+export default Author
