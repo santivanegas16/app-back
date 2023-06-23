1ad16ab3de887e2a890d8d4f802e801c3f2cb585
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-06-22 20:51:04 -0500

    se corrige controlador create

--- controllers/authors/create.js
+++ controllers/authors/create.js
@@ -2,21 +2,16 @@ import Author from "../../models/Author.js"
 
 export default async (req, res) => {
     try {
-        let data = req.body              //el cliente envia un objeto en la propiedad body del objeto req (requerimientos)
-        let one = Author.create(data)       //el objeto data deberia contener todos los datos necesarios para crear algo
-        if (one) {
+        let data = req.body              
+        let one = await Author.create(data)      
             return res.status(201).json({
+                   succes: false,
                 response: one,
                 message: 'created'
             })
-        } else {
-            return res.status(400).json({
-                response: null,
-                message: 'not created'
-            })
-        }
     } catch (error) {
         return res.status(500).json({
+            succes: false,
             response: null,
             message: 'not created'
         })
