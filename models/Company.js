d9e777b3ff88d2cbf9233b5799f10ad62e3e0d18
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-06-22 20:00:37 -0500

    Updating model Company

--- models/Company.js
+++ models/Company.js
@@ -1,12 +1,18 @@
-import {Schema, model} from 'mongoose'
+import {Schema, model, Types} from 'mongoose'
 
 let collection = 'companies'
 
 let schema = new Schema ({
     name: {type: String, required: true},
-    website: {type: String},
-    profile_photo: {type: String},
-    description: {type: String, required: true}
+    logo: {type: String, required:true},
+    website: {type: String, required:true},
+    description: {type: String, required: true},
+    active: { type:Boolean,required:true },
+    user_id: {
+        type: Types.ObjectId,
+        ref: 'users',
+        required: true
+    }
 },{
     timestamps: true
 })
