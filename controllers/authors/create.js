7ca12ae74cadc935f79a8dffb9f6c2a36e017e4f
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-06-22 21:02:49 -0500

    limpiar codigo

--- controllers/authors/create.js
+++ controllers/authors/create.js
@@ -2,13 +2,13 @@ import Author from "../../models/Author.js"
 
 export default async (req, res) => {
     try {
-        let data = req.body              
-        let one = await Author.create(data)      
-            return res.status(201).json({
-                   succes: false,
-                response: one,
-                message: 'created'
-            })
+        let data = req.body
+        let one = await Author.create(data)
+        return res.status(201).json({
+            succes: false,
+            response: one,
+            message: 'created'
+        })
     } catch (error) {
         return res.status(500).json({
             succes: false,
