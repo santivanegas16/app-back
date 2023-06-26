5acc64dce0a19b4d3408d5c04c14e96979742423
Juanmat1317 <89482278+Juanmat1317@users.noreply.github.com>
2023-06-25 22:48:15 -0500

    end-point is created (#12)

--- router/mangas.js
+++ router/mangas.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import read from "../controllers/mangas/read.js";
+import create from "../controllers/mangas/create.js";
 
 let mangasRouter = Router()
 
+mangasRouter.post('/',create)
 mangasRouter.get('/', read)
 
 export default mangasRouter
