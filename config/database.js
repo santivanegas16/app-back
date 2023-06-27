507bb6f24618792e2db724b1398a4adb40e22bb2
EstefaniaMarin <125941129+EstefaniaMarin@users.noreply.github.com>
2023-06-26 21:52:56 -0500

    se agregó en chapter create y en data chapteres y enrutamos chapter a create

--- config/database.js
+++ config/database.js
@@ -1,12 +1,5 @@
 import mongoose from "mongoose";
 
-async function  connect_db(){
-    try {
-        let link = process.env.MONGO_URI;
-        await mongoose.connect(link);
-        console.log('Database connect true');
-      } catch (error) {
-        console.log(error);
-      }
-}
-connect_db()
+let link = process.env.MONGO_URI;
+
+mongoose.connect(link).then(()=> console.log("database connected")).catch(error=> console.log(error))
