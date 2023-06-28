a5b432cdb0f8af0d029a9f5d2fb02e8b1845a6fa
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-06-27 20:37:20 -0500

    Updating middleware is_property_of

--- middlewares/is_property_of.js
+++ middlewares/is_property_of.js
@@ -5,18 +5,25 @@ import Manga from '../models/Manga.js'
 
 export default async (req, res, next ) => {
   try {
+    const {manga_id} = req.body
 
+    let manga = await Manga.findOne({
+      _id: manga_id
+    })
     let author_rid = req.author._id
     let company_rid = req.company._id
-    let manga = await Manga.findOne({})
 
-    if((author_rid === manga.author_id) || (company_rid === manga.company_id)) {
+    if(author_rid === manga.author_id) {
         return next()
     }
 
-    return res.status(404).json({
-        success: false,
-        message: 'the publisher who wants to create a chapter is not the same owner who create the manga!'
+    if(company_rid === manga.company_id) {
+      return next()
+    }
+
+    return res.status(400).json({
+        response: null,
+        message: 'Not authorized!'
     })
 
   } catch (error) {
