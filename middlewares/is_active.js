83ca80cb9968c0cf80649173fb1643a9e8c6d6ae
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-06-27 20:50:18 -0500

    Updating middleware is_active

--- middlewares/is_active.js
+++ middlewares/is_active.js
@@ -2,12 +2,12 @@
 export default (req, res, next) => {
     let author_active = req.author.active
     let company_active = req.company.active
-    console.log(req)
+
     if (author_active || company_active) {
         return next()
     }
-    return res.status(403).json({
-        success: false,
+    return res.status(400).json({
+        response: null,
         message: 'author or company does not active'
     })
 }
