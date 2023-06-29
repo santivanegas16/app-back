1e30510ed6687370cf8954148b1e33484089c676
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-06-29 00:06:40 -0500

    schema validate modified

--- Schemas/authors/create.js
+++ Schemas/authors/create.js
@@ -1,17 +1,46 @@
 import joi from "joi-oid"
 
 const schema = joi.object({
-    name: joi.string().min(3).max(20).messages({
-        'string.min':'tiene que tener al menos 3 caracteres',
-        'string.max':'tiene que tener menos de 20 caracteres'
+    name: joi.string().required().min(3).max(20).messages({
+        "string.base" : "Name must be string",
+        "string.empty" : "Name is required",
+        "any.required" : "Name is required",
+        'string.min' : "Name must be at least 3 characters long",
+        'string.max' : "Name must be a maximum of 20 characters"
     }),
-    last_name: joi.string(),
-    city: joi.string(),
-    country: joi.string(),
-    date: joi.date(),
-    photo: joi.string().uri(),
-    user_id: joi.objectId(),
-    active: joi.boolean()
+    last_name: joi.string().min(3).max(20).messages({
+        "string.base" : "Last name must be string",
+        'string.min' : "Last name must be at least 3 characters long",
+        'string.max' : "Last name must be a maximum of 20 characters"
+    }),
+    city: joi.string().required().messages({
+        "string.base" : "City must be string",
+        "string.empty" : "City is required",
+        "any.required" : "City is required"
+    }),
+    country: joi.string().required().messages({
+        "string.base" : "Country must be string",
+        "string.empty" : "Country is required",
+        "any.required" : "Country is required"
+    }),
+    date: joi.date().min('now').messages({
+        "date.base" : "Must be a valid date",
+        "date.min" : "Must be a minimum of the current date"
+    }),
+    photo: joi.string().uri().required().messages({
+        "any.required" : "Photo is required",
+        "string.uri" : "Photo must be an URL",
+        "string.base" : "Photo must be an URL",
+        "string.empty" : "Photo is required"
+    }),
+    user_id: joi.objectId().required().messages({
+        "any.required" : "Required"
+    }),
+    active: joi.boolean().required().messages({
+        "any.required" : "Required",
+        "boolean.base" : "Must be boolean",
+        "boolean.empty" : "Required"
+    })
 })
 
 export default schema
