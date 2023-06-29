15230f9def45f991025bd232452d7847f264542f
Jannus Alejandro Largo Zafra <96785247+JALZ2003@users.noreply.github.com>
2023-06-28 21:48:11 -0500

    All endpoints and middlewares for Sign in and Sign up were made (#20)

--- middlewares/generateToken.js
+++ middlewares/generateToken.js
@@ -1,8 +1,6 @@
 import jwt from 'jsonwebtoken';         // npm install jsonwebtoken
 
-export default (req, res, next) => {
-                            // objeto a convertir       clave secreta           expiracion del token
-    const token = jwt.sign({ email: req.user.email }, process.env.SECRET, { expiresIn: 60 * 60 * 24 * 7 });
-    req.token = token;
+export default (req, res, next) => {// objeto a convertir  clave secreta    expiracion del token
+    req.token = jwt.sign({ _id: req.user._id }, process.env.SECRET, { expiresIn: 60 * 60 * 24 * 7 });
     return next();
 }
--- middlewares/isValidPassword.js
+++ middlewares/isValidPassword.js
@@ -1,10 +1,9 @@
 import bcryptjs from 'bcryptjs';            // npm i bcryptjs
 
 export default (req, res, next) => {
-    const compare = bcryptjs.compareSync(req.body.password, req.user.password);     //compara las contraseñas y devuelte un booleano
-    if (compare) {
+    if (bcryptjs.compareSync(req.body.password, req.user.password)) {   //compara las contraseñas y devuelte un booleano
         delete req.user.password;
         return next();
     }
-    return res.status(400).json({ success: false, message: 'Invalid credentials!' }); 
+    return res.status(400).json({ success: false, message: ['Invalid credentials!'] });
 }
