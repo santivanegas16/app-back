975c39a37303d0e7eb75ed61af4ab36d0ee618bc
Estefanía Marín Vallejo <125941129+EstefaniaMarin@users.noreply.github.com>
2023-06-29 00:12:11 -0500

    M04 middlewares2 (#23)
    * add middleware add_cover_photo and route a chapter
    
    * add middleware add_cover_photo and route a chapter

--- router/chapters.js
+++ router/chapters.js
@@ -2,10 +2,11 @@ import { Router } from "express";
 import read from "../controllers/chapters/read.js";
 import create from "../controllers/chapters/create.js";
 import next_order from "../middlewares/next_order.js";
+import add_cover_photo from "../middlewares/add_cover_photo.js"
 
 let chaptersRouter = Router()
 
 chaptersRouter.get('/',read)
-chaptersRouter.post('/',next_order,create)
+chaptersRouter.post('/',next_order,add_cover_photo,create)
 
 export default chaptersRouter
