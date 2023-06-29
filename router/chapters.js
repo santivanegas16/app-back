0fec81c6c408ccc4941bde8f255c1180dbce01ef
Estefanía Marín Vallejo <125941129+EstefaniaMarin@users.noreply.github.com>
2023-06-28 23:47:24 -0500

    add middleware next_order and route a chapter (#21)

--- router/chapters.js
+++ router/chapters.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import read from "../controllers/chapters/read.js";
 import create from "../controllers/chapters/create.js";
+import next_order from "../middlewares/next_order.js";
 
 let chaptersRouter = Router()
 
 chaptersRouter.get('/',read)
-chaptersRouter.post('/',create)
+chaptersRouter.post('/',next_order,create)
 
 export default chaptersRouter
