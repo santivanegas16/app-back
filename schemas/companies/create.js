05dac16c3faea04f31dea0fb468ebeae5ef3ae78
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-07-05 19:41:10 -0500

    m05-alerts Updating schema for companies

--- schemas/companies/create.js
+++ schemas/companies/create.js
@@ -2,26 +2,30 @@ import joi from 'joi-oid'
 
 const schema = joi.object({
     name: joi.string().required().min(5).max(48).messages({
-        "string.empty" : "Name is not allowed to be empty",
         "any.required" : "Name is required",
+        "string.empty" : "Name is not allowed to be empty",
         "string.min" : "Name must be at least 5 characters long",
         "string.max" : "Name must be a maximum of 48 characters long",
+        "string.base" : "Name must be a string",
     }),
     logo: joi.string().uri().required().messages({
-        "string.empty" : "Logo is not allowed to be empty",
         "any.required" : "Logo is required",
-        "string.uri" : "Logo must be a valid URL"
+        "string.empty" : "Logo is not allowed to be empty",
+        "string.uri" : "Logo must be a valid URL",
+        "string.base" : "Logo must be a string",
     }),
     website: joi.string().uri().required().messages({
-        "string.empty" : "Website is not allowed to be empty",
         "any.required" : "Website is required",
+        "string.empty" : "Website is not allowed to be empty",
         "string.uri" : "Website must be a valid URL",
+        "string.base" : "Website must be a string",
     }),
     description: joi.string().required().min(5).max(255).messages({
-        "string.empty" : "Name is not allowed to be empty",
-        "any.required" : "Name is required",
+        "any.required" : "Description is required",
+        "string.empty" : "Description is not allowed to be empty",
         "string.min" : "Description must be at least 5 characters long",
         "string.max" : "Description must be a maximum of 255 characters long",
+        "string.base" : "Description must be a string",
     }),
     active: joi.boolean().messages({
         "boolean.base" : "Active must be a boolean",
