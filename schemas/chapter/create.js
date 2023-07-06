d3d42cb7c734723a27568fc6caf4a3b6e76e19ee
Estefanía Marín Vallejo <125941129+EstefaniaMarin@users.noreply.github.com>
2023-07-05 23:49:54 -0500

    create chapter and schema middlewares (#28)

---
+++ schemas/chapter/create.js
@@ -0,0 +1,25 @@
+import joi from 'joi-oid'
+
+const schema = joi.object({
+    title: joi.string()
+        .required()
+        .min(3)
+        .messages({
+            'any.required': 'Title is required!',
+            'string.base': 'Title is required!',
+            'string.empty': 'Title is required!',
+            'string.min': 'Title requires 3 characters!'
+        }),
+    pages: joi.array()
+        .required()
+        .items(joi.string().uri().min(10).messages({
+            'any.required': 'Page is required!',
+            'string.base': 'Page is required!',
+            'string.empty': 'Page is required!',
+            'string.min': 'Page requires 10 characters!',
+            'string.uri': 'Page must be a valid URL!'
+        })),
+    manga_id: joi.required()//.objectId()
+})
+
+export default schema
