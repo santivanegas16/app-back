c6d49b1d7d7848073a23beeb0b679eaefed711bd
Diana Carolina Hernandez Abahonza <dianacaro3552@gmail.com>
2023-07-05 19:30:05 -0500

    m05-alerts Updating model Company and its schema

--- router/companies.js
+++ router/companies.js
@@ -1,10 +1,20 @@
 import { Router } from "express"
 import read from '../controllers/companies/read.js'
 import create from '../controllers/companies/create.js'
+import validator from "../middlewares/validator.js";
+import schema_create from "../schemas/companies/create.js"
+import passport from "passport";
 
 let companiesRouter = Router()
 
-companiesRouter.get('/', read)
-companiesRouter.post('/', create)
+companiesRouter.get('/',
+    //passport.authenticate("jwt",{"session":false}),
+    read
+)
+companiesRouter.post('/',
+    //passport.authenticate("jwt",{"session":false}),
+    validator(schema_create),
+    create
+)
 
 export default companiesRouter
