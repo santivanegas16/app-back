545512763618230fce0fceec561933064a664dc1
Jannus Alejandro Largo Zafra <96785247+JALZ2003@users.noreply.github.com>
2023-07-06 08:24:01 -0500

    Solve problems messages in signin and register (#31)

--- controllers/auth/signin-token.js
+++ controllers/auth/signin-token.js
@@ -6,6 +6,6 @@ export default async (req, res, next) => {
         req.user = { email: user.email, photo: user.photo, role: user.role };
         return res.status(200).json({ response: { token: req.token, user: req.user }, message: 'User sigin with token' });
     } catch (error) {
-        return next(error);
+        return next();
     }
 }
