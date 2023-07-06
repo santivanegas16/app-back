8dd7822cb842f769f792151364618045bb9622fc
Juanmat1317 <89482278+Juanmat1317@users.noreply.github.com>
2023-07-05 22:17:26 -0500

    -03 alerts almost done

--- router/mangas.js
+++ router/mangas.js
@@ -3,10 +3,14 @@ import read from "../controllers/mangas/read.js";
 import create from "../controllers/mangas/create.js";
 import validator from "../middlewares/validator.js";
 import schema_create from "../schemas/mangas/create.js";
+import passport from "../middlewares/passport.js";
+import has_permition from "../middlewares/has_permition.js";
+import is_active from "../middlewares/is_active.js"
+
 
 let mangasRouter = Router()
 
-mangasRouter.post('/', validator(schema_create), create)
+mangasRouter.post('/', passport.authenticate("jwt",{"session":false}), validator(schema_create),has_permition,is_active, create)
 mangasRouter.get('/', read)
 
 export default mangasRouter
