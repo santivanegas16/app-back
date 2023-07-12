ab12ac895031d3e182dcffa723a167ef39780c02
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-07-11 21:31:57 -0500

    router modified

--- controllers/authors/read_me.js
+++ controllers/authors/read_me.js
@@ -1,22 +1,16 @@
-import Author from "../../models/Author.js"
+import Author from "../../models/Author.js";
 
-export default async (req, res, next) => {
-    try {
-        let consultas = {}
-        
-        let all = await Author.find()
-        if (all) {
-            return res.status(200).json({
-                response: all,
-                message: 'authors found!'
-            })
-        } else {
-            return res.status(404).json({
-                response: null,
-                message: 'authors not found'
-            })
-        }
-    } catch (error) {
-        next(error)
-    }
+export default async (req,res,next) => {
+try {
+    let id = req.params.id
+    console.log(id)
+    let profile = await Author.findById(id)
+    return res.status(200).json({
+        success: true,
+        response: profile
+    })
+} catch (error) {
+    next (error)
 }
+}
+
