cd07136828dcd82b6d88763f1a05bbdce83b196a
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-07-13 22:57:35 -0500

    endpoint read_news finished

--- controllers/mangas/read_news.js
+++ controllers/mangas/read_news.js
@@ -3,23 +3,28 @@ import Manga from "../../models/Manga.js"
 export default async (req, res, next) => {
     try {
         let all = await Manga.find({ author_id: req.author._id })
+        const datosOrdenados = all.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
         if (all.length < 4) {
             return res.status(200).json({
                 success: true,
                 response: { mangas: all, logo: "www.google.com" },
                 message: 'Mangas found!'
             })
-        } else if (all.length >= 4 && all.length < 8) {
-            
+        } else if (all.length >= 4 && all.length < 8) {       
+            const masViejos = datosOrdenados.slice(-2);
+            const masNuevos = datosOrdenados.slice(0, 2);
+            const resultado = [...masNuevos, ...masViejos];    
             return res.status(200).json({
                 success: true,
-                response: { all: all },
+                response: { all : resultado },
                 message: 'Mangas found!'
             })
         } else if (all.length >= 8) {
+            const masViejos = datosOrdenados.slice(-4);
+            const masNuevos = datosOrdenados.slice(0, 4);
             return res.status(200).json({
                 success: true,
-                response: { old: all, new: all },
+                response: { new: masNuevos, old: masViejos },
                 message: 'Mangas found!'
             })
         }
