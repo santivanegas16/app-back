96bcb222cc2ed8c8c95d27a3c4d9e8a18d56dea3
Juanmat1317 <89482278+Juanmat1317@users.noreply.github.com>
2023-07-13 23:51:04 -0500

    endpoint manga read one (#39)
    Co-authored-by: Santiago Vanegas Mejia <125935085+santivanegas16@users.noreply.github.com>

--- router/mangas.js
+++ router/mangas.js
@@ -2,9 +2,10 @@ import { Router } from "express";
 import read from "../controllers/mangas/read.js";
 import create from "../controllers/mangas/create.js";
 import validator from "../middlewares/validator.js";
-import schema_create from "../schemas/mangas/create.js";
+import schema_create from "../Schemas/mangas/create.js";
 import passport from "../middlewares/passport.js";
 import has_permition from "../middlewares/has_permition.js";
+import read_one from "../controllers/mangas/read_one.js";
 import is_active from "../middlewares/is_active.js"
 import read_news from "../controllers/mangas/read_news.js";
 
@@ -13,5 +14,6 @@ const mangasRouter = Router()
 mangasRouter.post('/', passport.authenticate("jwt", { "session": false }), validator(schema_create), has_permition, is_active, create);
 mangasRouter.get('/', passport.authenticate('jwt', { 'session': false }), read);
 mangasRouter.get('/news', passport.authenticate("jwt",{"session":false}), has_permition, read_news)
+mangasRouter.get('/:id', passport.authenticate("jwt",{"session":false}), read_one )
 
 export default mangasRouter;
