ea01aa4ecf486a911e2715acbc6728dea81a3100
EstefaniaMarin <125941129+EstefaniaMarin@users.noreply.github.com>
2023-07-30 13:35:16 -0500

    create read_one

---
+++ controllers/chapters/read_one.js
@@ -0,0 +1,19 @@
+import Chapter from "../../models/Chapter.js";
+
+export default async (req, res, next) => {
+  try {
+    const id = req.params.id;
+    const chapter = await Chapter.findById(id, "title cover_photo pages -_id");
+    const next = await Chapter.findOne({ _id: { $gt: id } }, "_id");
+    return res.status(200).json({
+      success: true,
+      response: {
+        chapter,
+        next: next ? next._id : null,
+      },
+      message: "Found",
+    });
+  } catch (error) {
+    return next(error);
+  }
+};
