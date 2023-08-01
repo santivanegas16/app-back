e678eed0f1fdc2352ec9ede23b4feac69bca3d7c
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-07-31 20:02:21 -0500

    fixed controller and schema

--- controllers/chapters/read.js
+++ controllers/chapters/read.js
@@ -1,33 +1,9 @@
-// import Chapter from '../../models/Chapter.js';
-
-// export default async (req, res) => {
-//     try {
-//         let all = await Chapter.find()
-//         if (all) {
-//             return res.status(200).json({
-//                 response: all,
-//                 message: 'chapter found'
-//             })
-//         } else {
-//             return res.status(404).json({
-//                 response: null,
-//                 message: 'chapter not found'
-//             });
-//         }
-//     } catch (error) {
-//         return res.status(500).json({
-//             response: null,
-//             error: 'error'
-//         })
-//     }
-// }
-
 import Chapter from '../../models/Chapter.js';
 
-export default async (req, res) => {
+export default async (req, res, next) => {
     try {
-        const{manga_id,page} = req.query;
-        const limit =6;
+        const { manga_id, page } = req.query;
+        const limit = 6;
         const pageNumber = parseInt(page) || 1;
 
         // Filtro por manga_id
@@ -36,7 +12,7 @@ export default async (req, res) => {
         // Contar la cantidad total de capítulos que coinciden con el manga_id
         const totalChapters = await Chapter.countDocuments(mangaFilter);
 
-        
+
         // Calcular el total de páginas
         const totalPages = Math.ceil(totalChapters / limit);
 
@@ -45,29 +21,29 @@ export default async (req, res) => {
 
         // Obtener los capítulos de la página actual
         const chapters = await Chapter.find(mangaFilter)
-        .select('-_id -__v') // Excluir campos que no se renderizarán en la vista
-        .sort({ order: 1 }) // Ordenar por orden ascendente
-        .skip(skip)
-        .limit(limit);
+            .select('-_id -__v') // Excluir campos que no se renderizarán en la vista
+            .sort({ order: 1 }) // Ordenar por orden ascendente
+            .skip(skip)
+            .limit(limit);
 
         // Configurar las propiedades estándar y la propiedad 'chapters' en la respuesta
         const response = {
-        totalPages,
-        currentPage: pageNumber,
-        chapters,
+            totalPages,
+            currentPage: pageNumber,
+            chapters,
         };
 
         // Configurar las propiedades 'prev' y 'next' en la respuesta
         if (pageNumber > 1) {
-        response.prev = pageNumber - 1;
+            response.prev = pageNumber - 1;
         }
         if (pageNumber < totalPages) {
-        response.next = pageNumber + 1;
+            response.next = pageNumber + 1;
         }
-        
-        res.status(200).json({response, message: "Chapters found"});
+
+        res.status(200).json({ response, message: "Chapters found" });
 
     } catch (error) {
         next(error)
     }
-    }
+}
--- schemas/companies/create.js
+++ schemas/companies/create.js
@@ -3,26 +3,26 @@ import joi from 'joi-oid'
 const schema = joi.object({
     name: joi.string().required().min(5).max(48).messages({
         "any.required" : "Name is required",
-        "string.empty" : "Name is not allowed to be empty",
+        "string.empty" : "Name is required",
         "string.min" : "Name must be at least 5 characters long",
         "string.max" : "Name must be a maximum of 48 characters long",
         "string.base" : "Name must be a string",
     }),
     logo: joi.string().uri().required().messages({
         "any.required" : "Logo is required",
-        "string.empty" : "Logo is not allowed to be empty",
+        "string.empty" : "Logo is required",
         "string.uri" : "Logo must be a valid URL",
         "string.base" : "Logo must be a string",
     }),
     website: joi.string().uri().required().messages({
         "any.required" : "Website is required",
-        "string.empty" : "Website is not allowed to be empty",
+        "string.empty" : "Website is required",
         "string.uri" : "Website must be a valid URL",
         "string.base" : "Website must be a string",
     }),
     description: joi.string().required().min(5).max(255).messages({
         "any.required" : "Description is required",
-        "string.empty" : "Description is not allowed to be empty",
+        "string.empty" : "Description is required",
         "string.min" : "Description must be at least 5 characters long",
         "string.max" : "Description must be a maximum of 255 characters long",
         "string.base" : "Description must be a string",
