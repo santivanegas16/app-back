1cd70579df27eabb97ba04f7bdcdb350f8b8f230
Juanmat1317 <89482278+Juanmat1317@users.noreply.github.com>
2023-08-11 15:51:03 -0500

    endpoint get is done

--- router/reactions.js
+++ router/reactions.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import create from "../controllers/reactions/create.js";
 import passport from "../middlewares/passport.js";
+import read from "../controllers/reactions/read.js";
 
 const reactionsRouter = Router()
 
 reactionsRouter.post('/',  passport.authenticate("jwt",{"session":false}),create)
+reactionsRouter.get('/', read)
 
 export default reactionsRouter
