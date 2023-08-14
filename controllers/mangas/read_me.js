3fc2c6fe7903770e3c3adf324700f78b1dc24cff
natigalo <natigalo1@gmail.com>
2023-08-13 21:20:12 -0500

    endpoints read-me, update and destroy

--- controllers/mangas/read_me.js
+++ controllers/mangas/read_me.js
@@ -1,16 +1,24 @@
  import Manga from '../../models/Manga.js';
 
-export default async function read_me(req, res, next) {
+export default async (req, res, next) =>{
     try {
+        console.log(req.body)
         let all = await Manga.find({author_id: req.body.author_id})
+        
         if(all.length> 0){
             return res.status(200).json({
-                /* success: true,
-                response: { all : resultado },
-                message: 'Mangas found!' */
+                response: all,
+                success: true,
+                message: 'Mangas found!' 
+            })
+        }else{
+            return res.satatus(404).json({
+                success: false,
+                response: null,
+                message: 'mangas not found'
             })
         }
     } catch (error) {
-        console.log(error)
+        next(error)
     }
 } 
