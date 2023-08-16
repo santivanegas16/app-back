a4148b1414cc40a7d77d6fd227408d298b034753
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-08-15 23:11:22 -0500

    fix read_me

--- controllers/mangas/read_me.js
+++ controllers/mangas/read_me.js
@@ -1,30 +1,5 @@
 import Manga from '../../models/Manga.js';
 
-// export default async (req, res, next) => {
-//     try {
-//         console.log(req.body)
-//         const queries = { author_id: req.body.author_id };
-//         if (req.query.category) { queries.category_id = req.query.category.split(',') }
-
-//         let all = await Manga.find(queries, "title cover_photo description").populate("category_id", "name color hover -_id")
-//         if (all.length > 0) {
-//             return res.status(200).json({
-//                 response: all,
-//                 success: true,
-//                 message: 'Mangas found!'
-//             })
-//         } else {
-//             return res.satatus(404).json({
-//                 success: false,
-//                 response: null,
-//                 message: 'mangas not found'
-//             })
-//         }
-//     } catch (error) {
-//         next(error)
-//     }
-// }
-
 export default async (req, res, next) => {
     try {
         const queries = { author_id: req.body.author_id };
@@ -32,7 +7,6 @@ export default async (req, res, next) => {
         const pagination = { page: 1, limit: 4 };
         
         if (req.query.category) { queries.category_id = req.query.category.split(',')} 
-        if (req.query.title) { queries.title = new RegExp(req.query.title.trim(), 'i') }
         if (req.query.sort) { ordering.title = Number(req.query.sort) }
         if (req.query.page) { pagination.page = Number(req.query.page) }
         if (req.query.quantity) { pagination.limit = Number(req.query.quantity) }
