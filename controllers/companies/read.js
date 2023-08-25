8e6896a4f7300b111417e211695930b8f3de865a
Gerardo  Aguirre <rg.aguirre93@gmail.com>
2023-08-24 21:42:50 -0500

    fix

--- controllers/companies/read.js
+++ controllers/companies/read.js
@@ -1,23 +1,21 @@
 import Company from "../../models/Company.js"
 
-export default async (req, res) => {
+export default async (req, res, next) => {
     try {
-        let all = await Company.find()
-        if (all) {
+        let active_true = await Company.find({ active: true })
+        let active_false = await Company.find({ active: false })
+        if (active_true || active_false) {
             return res.status(200).json({
-                response: all,
+                response: { active_true, active_false },
                 message: 'companies found!'
             })
         } else {
             return res.status(404).json({
                 response: null,
-                message: 'companies not found!'
+                message: 'companies not found'
             })
         }
     } catch (error) {
-        return res.status(500).json({
-            response: null,
-            message: error
-        })
+        next(error)
     }
 }
