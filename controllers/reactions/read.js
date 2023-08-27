866c602c4e68cc4478686e6d8ce4368707eb89bd
Juanmat1317 <89482278+Juanmat1317@users.noreply.github.com>
2023-08-27 15:57:02 -0500

    endpoints read and create are done

--- controllers/reactions/read.js
+++ controllers/reactions/read.js
@@ -2,38 +2,72 @@ import Reaction from "../../models/Reaction.js";
 
 export default async (req, res, next) => {
     try {
-        const queries = {}
-        if (req.query.manga_id) {
-            queries.manga_id = req.query.manga_id
-        }
-        if (req.query.user_id) {
-            queries.user_id = req.query.user_id
-        }
-        const reactions = await Reaction.find({ manga_id: queries.manga_id })
-        const likes = reactions.filter(each=>each.name==='like').length
-        const dislikes = reactions.filter(each=>each.name==='dislike').length
-        let likeUser = false
-        let dislikeUser = false
-        if (queries.user_id) {
-            const reacted = await Reaction.find(queries)
-            reacted.name === 'like' ? likeUser=true : dislikeUser=true
-        }
+        if (req.query.name) {
+            const queries = { user_id: req.user._id, name: req.query.name };
+            console.log(queries);
+            const reactions = await Reaction.findOne(queries);
+            if (reactions) {
+                return res.status(200).json({
+                    success: true,
+                    response: reactions,
+                    message: 'found'
+                });
+            }else{
+                return res.status(404).json({
+                    success: false,
+                    response: null,
+                    message: 'not found'
+                });
+            }
+           
+        } else if (req.query.manga_id || req.query.user_id) {
+            const queries = {};
+            if (req.query.manga_id) {
+                queries.manga_id = req.query.manga_id;
+            }
+            if (req.query.user_id) {
+                queries.user_id = req.query.user_id;
+            }
 
-        return res.status(200).json({
-            success: true,
-            response: {
-                reactions:{
-                    likes,
-                    dislikes
-                },
-                reacted:{
-                    like: likeUser,
-                    dislike: dislikeUser
+            const reactions = await Reaction.find({ manga_id: queries.manga_id });
+            const likes = reactions.filter(each => each.name === 'like').length;
+            const dislikes = reactions.filter(each => each.name === 'dislike').length;
+            let likeUser = false;
+            let dislikeUser = false;
+
+            if (queries.user_id) {
+                const reacted = await Reaction.findOne(queries);
+                if (reacted.name === 'like') {
+                    likeUser = true;
+                } else {
+                    dislikeUser = true;
                 }
-            },
-            message: 'found'
-        })
+            }
+
+            return res.status(200).json({
+                success: true,
+                response: {
+                    reactions: {
+                        likes,
+                        dislikes
+                    },
+                    reacted: {
+                        like: likeUser,
+                        dislike: dislikeUser
+                    }
+                },
+                message: 'found'
+            });
+        } else {
+            return res.status(400).json({
+                success: false,
+                response: null,
+                message: 'not found'
+            });
+        }
     } catch (error) {
-        next(error)
+        next(error);
     }
-}
+};
+
+
--- router/reactions.js
+++ router/reactions.js
@@ -2,12 +2,11 @@ import { Router } from "express";
 import create from "../controllers/reactions/create.js";
 import passport from "../middlewares/passport.js";
 import read from "../controllers/reactions/read.js";
-import read_user from "../controllers/reactions/read_user.js";
+
 
 const reactionsRouter = Router()
 
 reactionsRouter.post('/',  passport.authenticate("jwt",{"session":false}),create)
-reactionsRouter.get('/',  passport.authenticate("jwt",{"session":false}), read_user)
-reactionsRouter.get('/', read)
+reactionsRouter.get('/',  passport.authenticate("jwt",{"session":false}), read)
 
 export default reactionsRouter
